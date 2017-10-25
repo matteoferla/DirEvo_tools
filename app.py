47820bff3afd9e17052ee8b6571bac0eca69eb93
Matteo Ferla <matteo.ferla@gmail.com>
2017-10-25 19:50:37 +0100

    :link: improving site interconnectivity...

--- app.py
+++ app.py
@@ -23,12 +23,13 @@ if __name__ == '__main__':
                                'main')  # don't really need this but is an example on how to get settings from the '.ini' files
 
     # Waitress
+
+    # did I change the C++ scripts. Let's recompile anyway!
+    os.system('bash '+os.path.join(here,'bikeshed/compile.sh'))
+    print('C++ compiled')
+
     from waitress import serve
 
     print("Starting Waitress.")
     print("working directory: ",os.getcwd())
     serve(app, host=ip, port=port, threads=50)
-
-    # did I change the C++ scripts. Let's recompile anyway!
-    os.system('bash '+os.path.join(here,'bikeshed/compile.sh'))
-    print('C++ compiled')
