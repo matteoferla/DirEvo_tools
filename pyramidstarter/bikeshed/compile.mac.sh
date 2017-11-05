068d803fd5313d9f06b19f58553c351c4b076a73
Matteo Ferla <matteo.ferla@gmail.com>
2017-11-05 18:45:13 +0000

    :hammer: compile error on server...

---
+++ pyramidstarter/bikeshed/compile.mac.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+os="mac"
+g++ -o driver.batch.$os driver.batch.cxx
+g++ -o driver.$os driver.cxx
+g++ -o glue.$os glue.cxx
+g++ -o pedel.batch.$os pedel.batch.cxx
+g++ -o pedel_mc.$os pedel_mc.cxx
+g++ -o driver_mc.$os driver_mc.cxx
+g++ -o glue_mc.$os glue_mc.cxx
+g++ -o pedel.$os pedel.cxx
+g++ -o stats.batch.$os stats.batch.cxx
+g++ -o glue1AAc glue1AAc.cxx
+g++ -o glueITc glueITc.cxx
+g++ -o glueNNS glueNNS.cxx
+g++ -o pedel-AAc pedel-AAc.cxx
+g++ -o stats.batch.mod.$os stats.batch.mod.cxx
+g++ -o glue.mod.$os glue.mod.cxx
--- pyramidstarter/bikeshed/compile.sh
+++ pyramidstarter/bikeshed/compile.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 os="linux"
-g++ -o driver.batch.$os driver.batch.cxx
-g++ -o driver.$os driver.cxx
-g++ -o glue.$os glue.cxx
-g++ -o pedel.batch.$os pedel.batch.cxx
-g++ -o pedel_mc.$os pedel_mc.cxx
-g++ -o driver_mc.$os driver_mc.cxx
-g++ -o glue_mc.$os glue_mc.cxx
-g++ -o pedel.$os pedel.cxx
-g++ -o stats.batch.$os stats.batch.cxx
-g++ -o glue1AAc glue1AAc.cxx
-g++ -o glueITc glueITc.cxx
-g++ -o glueNNS glueNNS.cxx
-g++ -o pedel-AAc pedel-AAc.cxx
-g++ -o stats.batch.mod.$os stats.batch.mod.cxx
-g++ -o glue.mod.$os glue.mod.cxx
+for i in $( ls /opt/app-root/src/pyramidstarter/bikeshed/*.cxx);
+do
+g++ -o "${i%.*}".$os "${i%.*}".cxx;
+done
