00cfc37a66a2953912f922757fd05e38e1805cd3
Matteo Ferla <matteo.ferla@gmail.com>
2018-08-23 16:09:55 +0100

    :pencil2: fixed update

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 git pull https://github.com/matteoferla/pedel2.git;
 kill $(ps aux | grep 'app.py' | awk '{print $2}');
-cd pyramidstarter/bikeshed
-sh compile.sh linux;
-cd ../..;
-chmod -R 777 .
+#cd pyramidstarter/bikeshed
+#sh compile.sh linux;
+#cd ../..;
+#chmod -R 777 .
 nohup python3 app.py > pyramidstarter/static/bash_log.txt &
