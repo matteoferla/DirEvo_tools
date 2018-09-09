16b7056145e2f620ed6386ef73d4b9cfccc80241
Matteo Ferla <matteo.ferla@gmail.com>
2018-09-09 12:04:32 +0100

    :alien: tidyed up update script

--- update.sh
+++ update.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+echo '********** UPDATE *****************';
 git pull https://github.com/matteoferla/pedel2.git;
+
+echo '********** KILL *****************';
 #kill $(ps aux | grep 'app.py' | awk '{print $2}'); # kill port 8080 only...
 kill $(sudo netstat -ltnp | grep ':8080' | awk '{split($7,a,"/"); print a[1]}')
+
+#echo '********** COMPILE *****************';
 #cd pyramidstarter/bikeshed
 #sh compile.sh linux;
 #cd ../..;
 #chmod -R 777 .;
+
+echo '********** RUN *****************';
 nohup python3 app.py > pyramidstarter/static/bash_log.txt &
 echo 'All done!';
 echo 'Having trouble: cat pyramidstarter/static/bash_log.txt';
